f3dff070b53b4a3c838fc86a379cd5d1981946c8
AloisioFernandes <aloisio.fernandes@outlook.com>
2025-08-20 07:19:35 -0300

    feat: add dotenv configuration and environment variable validation.

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { app } from "./app.js";
+import { env } from "./env/index.js";
 
-app.listen({ host: '0.0.0.0', port: 3333 }).then(() => {
-  console.log('🚀 Server is running on http://localhost:3333');
+app.listen({ host: '0.0.0.0', port: env.PORT }).then(() => {
+  console.log(`🚀 Server is running on http://localhost:${env.PORT}`);
 })
