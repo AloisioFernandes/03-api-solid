786aa0c8b9933895e28f0ffe97ade2334113285d
AloisioFernandes <aloisio.fernandes@outlook.com>
2025-08-30 11:48:53 -0300

    feat: refactor user registration logic into a separate use case service.

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,5 +1,4 @@
-import { prisma } from "@/db/prisma.js";
-import { hash } from "bcryptjs";
+import { registerUseCase } from "@/services/register.js";
 import type { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 
@@ -12,25 +11,11 @@ export async function register (request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body);
 
-  const password_hash = await hash(password, 6)
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email
-    }
-  })
-
-  if (userWithSameEmail) {
-    return reply.status(409).send()
+  try {
+    await registerUseCase({ name, email, password });
+  } catch (error) {
+    return reply.status(409).send();
   }
-
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
-  });
 
   return reply.status(201).send();
 }
