37f646fb4c038f75d97b9269e335a940da8e18f5
AloisioFernandes <aloisio.fernandes@outlook.com>
2025-09-09 07:30:58 -0300

    feat: add GymsRepository interface and InMemoryGymsRepository implementation; integrate dayjs for date handling in CheckInsRepository

--- src/repositories/memory/in-memory-check-ins-repository.ts
+++ src/repositories/memory/in-memory-check-ins-repository.ts
@@ -1,4 +1,5 @@
 import type { CheckIn, Prisma } from "@prisma/client";
+import dayjs from "dayjs";
 import { randomUUID } from "node:crypto";
 import type { CheckInsRepository } from "../check-ins-repository.js";
 
@@ -9,8 +10,16 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     userId: string,
     date: Date
   ): Promise<CheckIn | null> {
+    const startOfTheDay = dayjs(date).startOf("date");
+    const endOfTheDay = dayjs(date).endOf("date");
+
     const checkInOnSameDate = this.items.find(
-      (checkIn) => checkIn.user_id === userId
+      (checkIn) => {
+        const checkInDate = dayjs(checkIn.createdAt);
+        const isOnSameDate = checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay);
+        
+        return checkIn.user_id === userId && isOnSameDate;
+      }
     );
 
     if (!checkInOnSameDate) {
