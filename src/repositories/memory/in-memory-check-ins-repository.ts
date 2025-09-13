992e81c680fc94f6f99894942adcb1638e8fa4c4
AloisioFernandes <aloisio.fernandes@outlook.com>
2025-09-13 08:57:33 -0300

    feat: implement FetchUserCheckInsHistoryUseCase and associated tests; refactor CheckInsRepository methods

--- src/repositories/memory/in-memory-check-ins-repository.ts
+++ src/repositories/memory/in-memory-check-ins-repository.ts
@@ -13,20 +13,25 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     const startOfTheDay = dayjs(date).startOf("date");
     const endOfTheDay = dayjs(date).endOf("date");
 
-    const checkInOnSameDate = this.items.find(
-      (checkIn) => {
-        const checkInDate = dayjs(checkIn.createdAt);
-        const isOnSameDate = checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay);
-        
-        return checkIn.user_id === userId && isOnSameDate;
-      }
-    );
+    const checkInOnSameDate = this.items.find((checkIn) => {
+      const checkInDate = dayjs(checkIn.createdAt);
+      const isOnSameDate =
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay);
+
+      return checkIn.user_id === userId && isOnSameDate;
+    });
 
     if (!checkInOnSameDate) {
       return null;
     }
 
     return checkInOnSameDate;
+  }
+
+  async findManyByUserId(userId: string, page: number): Promise<CheckIn[]> {
+    return this.items
+      .filter((item) => item.user_id === userId)
+      .slice((page - 1) * 20, page * 20);
   }
 
   async create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn> {
