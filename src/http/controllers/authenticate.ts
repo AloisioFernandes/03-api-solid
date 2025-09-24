2c6c0e346d6f78cf7535b82ae225f982009f8f6f
AloisioFernandes <aloisio.fernandes@outlook.com>
2025-09-24 07:23:06 -0300

    feat: integrate JWT authentication with Fastify; add JWT_SECRET to env and update profile controller to verify JWT

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -15,9 +15,20 @@ export async function authenticate(
   const { email, password } = authenticateBodySchema.parse(request.body);
 
   try {
-    const authenticateService = makeAuthenticateService()
+    const authenticateService = makeAuthenticateService();
 
-    await authenticateService.execute({ email, password });
+    const { user } = await authenticateService.execute({ email, password });
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      }
+    );
+
+    return reply.status(200).send({ token });
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: error.message });
@@ -25,6 +36,4 @@ export async function authenticate(
 
     throw error;
   }
-
-  return reply.status(200).send();
 }
