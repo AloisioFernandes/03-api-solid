7cccbdf2d33e723b6db90ee8443aa4033e0ed67e
AloisioFernandes <aloisio.fernandes@outlook.com>
2025-10-07 07:56:10 -0300

    feat: add @fastify/cookie for JWT refresh token handling and update authentication flow

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -28,7 +28,25 @@ export async function authenticate(
       }
     );
 
-    return reply.status(200).send({ token });
+    const refreshToken = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: "7d",
+        },
+      }
+    );
+
+    return reply
+      .setCookie("refreshToken", refreshToken, {
+        path: "/", // Quais rotas terão acesso ao cookie
+        secure: true, // Só funciona em conexões HTTPS
+        sameSite: true, // Impede que sites de terceiros tenham acesso ao cookie
+        httpOnly: true, // Impede que o JavaScript do navegador tenha acesso ao cookie
+      })
+      .status(200)
+      .send({ token });
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: error.message });
